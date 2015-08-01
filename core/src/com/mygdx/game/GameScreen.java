e1522912d26e01db18737d619efa98ef4221e472
bananasinpajamas <andrewstyll@gmail.com>
2015-08-01 15:54:57 -0400

    Load ALL the textures

--- core/src/com/mygdx/game/GameScreen.java
+++ core/src/com/mygdx/game/GameScreen.java
@@ -6,12 +6,16 @@ import com.badlogic.gdx.graphics.GL20;
 import com.badlogic.gdx.graphics.OrthographicCamera;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.utils.Array;
 
 public class GameScreen implements Screen {
     final BlackjackGame game;
 
     private OrthographicCamera camera;
-    private Texture cardImage;
+    //private Texture cardImage;
+
+    private Array<Array<Texture>> cardTextures;
+
     private Sprite card;
 
     public GameScreen(final BlackjackGame _game) {
@@ -20,8 +24,19 @@ public class GameScreen implements Screen {
         camera = new OrthographicCamera();
         camera.setToOrtho(false, 800, 480);
 
-        cardImage = new Texture(Gdx.files.internal("c12.png"));
-        card = new Sprite(cardImage);
+        //cardImage = new Texture(Gdx.files.internal("c12.png"));
+        cardTextures = new Array<Array<Texture>>(4);
+        String[] suits = {"spades", "hearts", "clubs", "diamonds"};
+        for(int j = 0; j < 4; j++) {
+            //this will grow this array from the first dimension onwards (implicit dimension)
+            //from the first index onwards
+            cardTextures.add(new Array<Texture>(13));
+            for(int i = 1; i <= 13; i++) {
+                String filename = suits[j].charAt(0) + String.format("%02d", i) + ".png";
+                cardTextures.get(j).add(new Texture(Gdx.files.internal(filename)));
+            }
+        }
+        card = new Sprite(cardTextures.get(3).get(5));
         float scale = 150 / card.getWidth();
         card.setSize(150, card.getHeight() * scale);
         card.setPosition(0, 0);
@@ -68,6 +83,10 @@ public class GameScreen implements Screen {
 
     @Override
     public void dispose() {
-        cardImage.dispose();
+        for(int j = 0; j < 4; j++) {
+            for(int i = 0; i < 13; i++) {
+                cardTextures.get(j).get(i).dispose();
+            }
+        }
     }
 }
